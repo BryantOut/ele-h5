682704d7649e572ec5548567004a6cdfad67c52a
bryantout <bryantout@163.com>
2023-09-04 16:06:33 +0800

    feat: 页面头部实现

--- src/views/tabs/home/HomeView.vue
+++ src/views/tabs/home/HomeView.vue
@@ -1,9 +1,22 @@
+<script setup lang="ts">
+import TheTop from './components/TheTop.vue';
+
+const recomments = [
+  {
+    value: 1,
+    label: '牛腩',
+  },
+  {
+    value: 2,
+    label: '色拉',
+  },
+];
+</script>
+
 <template>
-  <div>home</div>
+  <div class="home-page">
+    <TheTop :recomments="recomments" />
+  </div>
 </template>
 
-<style scoped>
-div {
-  font-size: 39px;
-}
-</style>
+<style scoped></style>
